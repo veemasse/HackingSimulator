4058a3ed279901c86914d1ee5e23634b09281e67
Vänttinen Matias Otto <matias.vanttinen@edu.tampere.fi>
2021-12-08 15:05:35 +0200

    Terminal works, can't scroll

---
+++ Assets/Scripts/Interpreter.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class Interpreter : MonoBehaviour
+{
+    Dictionary<string, string> colors = new Dictionary<string, string>()
+    {
+        {"green", "#008000"},
+        {"red", "#FF0000"}
+    };
+
+    bool vpn = false;
+    float pcpower;
+
+    string ip = "176.96.154.32 Reutte";
+    string cpu ="CPU: Intel® Pentium® III @ 400 MHz";
+    string mem ="Memory: 32MB DDR-200";
+    string mobo ="Motherboard: ASUS TUSL2-C rev. 1.04 Socket 370 Motherboard";
+    string gpu ="GPU: Nvidia Riva TNT";
+    string hdd ="HDD: 8.4GB";
+    
+    List<string> response = new List<string>();
+    public List<string> Interpret(string userInput)
+    {
+        response.Clear();
+
+        string[] args = userInput.Split();
+
+        //Return info about the command
+        if (args[0] == "help")
+        {
+            response.Add("Help");
+            response.Add("IPCONFIG");
+            response.Add("VPN");
+            return response;
+        }
+        if (args[0] == "test")
+        {
+            ListEntry("test", "yeees");
+            ListEntry("stop", "yes");
+            return response;
+        }
+        if (args[0] == "ascii")
+        {
+            LoadTitle("ascii.txt", "red", 2);
+            return response;
+        }
+        if (args[0] == "sysinfo" || args[0] == "systeminfo")
+        {
+            response.Add(cpu);
+            response.Add(mem);
+            response.Add(mobo);
+            response.Add(gpu);
+            response.Add(hdd);
+            return response;
+        }
+        if (args[0] == "ipconfig")
+        {
+            response.Add("IPv4: " + ip);
+            return response;
+        }
+        if (args[0] == "vpn")
+        {
+            if (vpn == false)
+            {
+                response.Add("Vpn Connected");
+                ip = "216.58.210.131 Helsinki";
+                Debug.Log(ip);
+                vpn = true;
+                Debug.Log(vpn);
+            }
+            else if (vpn == true)
+            {
+                response.Add("Vpn Offline");
+                ip = "176.96.154.32 Reutte";
+                vpn = false;
+                Debug.Log(vpn);
+            }
+            return response;
+        }
+        else
+        {
+            response.Add("Komentoa ei tunnistettu, kokeile komentoa help,");
+            response.Add("jotta näät saatavilla olevat komennot.");
+            return response;
+        }
+    }
+
+    public string ColorString(string s, string color)
+    {
+        string leftTag = "<color=" + color + ">";
+        string rightTag = "</color>";
+        return leftTag + s + rightTag;
+    }
+
+    void ListEntry(string a, string b)
+    {
+        response.Add(ColorString(a, colors["green"]) + ": " + ColorString(b, colors["red"]));
+    }
+    
+    void LoadTitle(string path, string color, int spacing)
+    {
+        StreamReader file = new StreamReader(Path.Combine(Application.streamingAssetsPath, path));
+
+        for(int i = 0; i < spacing; i++ )
+        {
+            response.Add("");
+        }
+
+        while(!file.EndOfStream)
+        {
+            response.Add(ColorString(file.ReadLine(), colors[color]));
+        }
+
+        for(int i = 0; i < spacing; i++ )
+        {
+            response.Add("");
+        }
+
+        file.Close();
+
+    }
+}
---
+++ Assets/Scripts/TerminalManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TerminalManager : MonoBehaviour
+{
+    public GameObject directoryLine;
+    public GameObject responseLine;
+
+    public InputField terminalInput;
+    public GameObject userInputLine;
+    public ScrollRect sr;
+    public GameObject msgList;
+
+    Interpreter interpreter;
+
+    private void Start()
+    {
+        interpreter = GetComponent<Interpreter>();
+    }
+
+    private void OnGUI()
+    {
+        if (terminalInput.isFocused && terminalInput.text != "" && Input.GetKeyDown(KeyCode.Return))
+        {
+            //tallentaa mitä komtentoriville kirjoitetaan.
+            string userInput = terminalInput.text;
+
+            //tyhjentää terminaalin
+            ClearInputField();
+
+            AddDirectoryLine(userInput);
+
+            //Add the interpretation lines
+            int lines = AddInterpreterLines(interpreter.Interpret(userInput));
+
+            //Scroll to the bottom of the scrollrect.
+            ScrollToBottom(lines);
+
+            //Move the user input line to the end.
+            userInputLine.transform.SetAsLastSibling();
+
+            //refocus the input field
+            terminalInput.ActivateInputField();
+            terminalInput.Select();
+        }
+    }
+
+    void ClearInputField()
+    {
+        terminalInput.text = "";
+    }
+
+    void AddDirectoryLine(string userInput)
+    {
+        //Muokkaa sisällön kokoa.
+        Vector2 msgListSize = msgList.GetComponent<RectTransform>().sizeDelta;
+        msgList.GetComponent<RectTransform>().sizeDelta = new Vector2(msgListSize.x, msgListSize.y + 35.0f);
+
+        GameObject msg = Instantiate(directoryLine, msgList.transform);
+
+        msg.transform.SetSiblingIndex(msgList.transform.childCount - 1);
+
+        //Set the text of this new gameobject
+        msg.GetComponentsInChildren<Text>()[1].text = userInput;
+    }
+    int AddInterpreterLines(List<string> interpretation)
+    {
+        for (int i = 0; i < interpretation.Count; i++)
+        {
+            //response line
+            GameObject res = Instantiate(responseLine, msgList.transform);
+
+            res.transform.SetAsLastSibling();
+            
+            //Get the size of message list
+            Vector2 listSize = msgList.GetComponent<RectTransform>().sizeDelta;
+            msgList.GetComponent<RectTransform>().sizeDelta = new Vector2(listSize.x, listSize.y + 35.0f);
+
+            //Set the text of this response line to be whatever the intepreter string is.
+            res.GetComponentInChildren<Text>().text = interpretation[i];
+        }
+
+        return interpretation.Count;
+    }
+
+    void ScrollToBottom(int lines)
+    {
+        if (lines > 4)
+        {
+            sr.velocity = new Vector2(0, 450);
+        }
+        else
+        {
+            sr.verticalNormalizedPosition = 0;
+        }
+    }
+}
